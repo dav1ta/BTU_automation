5c35893e3206aea8453a112cd0c0bb098815e064
dav1ta <daviti.tchanturia.1@btu.edu.ge>
2022-10-02 23:03:18 +0400

    click dropdown

---
+++ src/test/java/ExampleTests.java
@@ -0,0 +1,27 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ExampleTests {
+
+    @Test
+    public void firstTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        WebElement button=driver.findElement(By.xpath("//*[@id='content']/ul/li[11]/a"));
+        button.click();
+        WebElement dropdownElement = driver.findElement(By.xpath("//*[@id='dropdown']"));
+        Select dropdown = new Select(dropdownElement);
+        dropdown.selectByValue("1");
+        
+
+
+    }
+}
