373756b7b27b027933f7dd2107f9fe56996fa5b5
dav1ta <daviti.tchanturia.1@btu.edu.ge>
2022-10-03 20:18:26 +0400

    add format

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -9,23 +9,20 @@ import org.testng.annotations.Test;
 
 public class ExampleTests {
 
-    @Test
-    public void firstTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        WebElement button=driver.findElement(By.xpath("//ul/li/a"));
-        button.click();
-        driver.navigate().back();
-        WebElement button1=driver.findElement(By.xpath("//*[@id='content']/ul/li[11]/a"));
-        button1.click();
-        WebElement dropdownElement = driver.findElement(By.xpath("//*[@id='dropdown']"));
-        Select dropdown = new Select(dropdownElement);
-        dropdown.selectByValue("1");
-        
-    
+  @Test
+  public void firstTest() {
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://the-internet.herokuapp.com/");
+    driver.manage().window().maximize();
+    WebElement button = driver.findElement(By.xpath("//ul/li/a"));
+    button.click();
+    driver.navigate().back();
+    WebElement button1 = driver.findElement(By.xpath("//*[@id='content']/ul/li[11]/a"));
+    button1.click();
+    WebElement dropdownElement = driver.findElement(By.xpath("//*[@id='dropdown']"));
+    Select dropdown = new Select(dropdownElement);
+    dropdown.selectByValue("1");
 
-
-    }
+  }
 }
